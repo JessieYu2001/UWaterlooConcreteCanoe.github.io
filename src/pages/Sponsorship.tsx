b883cba3ff1a9910fe3aff9edc18ff5b7f5a25fc
JessieYu2001 <65326501+JessieYu2001@users.noreply.github.com>
2023-11-16 03:25:17 +0000

    add Sponsorship page

---
+++ src/pages/Sponsorship.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Sponsorship() {
+    return (
+        <div className="Sponsorship" style={{marginTop: "50px"}}>
+        xxx
+        </div>
+    );
+}
+
+export default Sponsorship;
